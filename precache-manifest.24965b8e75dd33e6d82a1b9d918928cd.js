515f85081ee84b0f28bd4604528cda719bd23d90
tawat chai <tawat1838@gmail.com>
2019-08-09 16:09:42 +0700

    Updates

---
+++ precache-manifest.24965b8e75dd33e6d82a1b9d918928cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7208f5570f147ebbcba",
+    "url": "/newffcin/static/js/runtime~main.ba404c56.js"
+  },
+  {
+    "revision": "08d3317e81165be42972",
+    "url": "/newffcin/static/js/main.861a0c6b.chunk.js"
+  },
+  {
+    "revision": "5faba77ff92d12e22617",
+    "url": "/newffcin/static/js/2.8aa6456a.chunk.js"
+  },
+  {
+    "revision": "08d3317e81165be42972",
+    "url": "/newffcin/static/css/main.ecf8b748.chunk.css"
+  },
+  {
+    "revision": "5faba77ff92d12e22617",
+    "url": "/newffcin/static/css/2.7bed68d0.chunk.css"
+  },
+  {
+    "revision": "b0adb870a71d0dcf860c7f3381b65861",
+    "url": "/newffcin/index.html"
+  }
+];
